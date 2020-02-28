6a6884ebd69415273a35de77d89fc779b2512046
Sreniok <sreniok@gmail.com>
2020-02-28 19:47:02 +0000

    heroku deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn graphic_design.wsgi.application
+web: gunicorn graphic_design.wsgi:application
--- graphic_design/settings.py
+++ graphic_design/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['graphic-designe-art.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'graphic-designe-art.herokuapp.com']
 
 
 # Application definition
