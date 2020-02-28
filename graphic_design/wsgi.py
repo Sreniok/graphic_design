a47529eb90c82ea6d5d18e9dffbdb1f57b62ce51
Sreniok <sreniok@gmail.com>
2020-02-28 19:53:59 +0000

    heroku deployment

--- graphic_design/wsgi.py
+++ graphic_design/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphic_design.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphic_design.settings.py")
 
 application = get_wsgi_application()
