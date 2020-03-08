2e7c19d51688f0f6f19eea89bd6643e227033dd4
Sreniok <sreniok@gmail.com>
2020-03-08 16:14:04 +0000

    add project information when paying

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,8 @@ from projects.models import Projects
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
+    project_information1 = models.TextField(blank=True)
+    project_information2 = models.TextField(blank=True)
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
     town_or_city = models.CharField(max_length=40, blank=False)
